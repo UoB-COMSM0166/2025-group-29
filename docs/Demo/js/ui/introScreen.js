e926a17c562fa147fc501127d1866d871a2e1538
Zeng Weihao <Zengweihaooo@gmail.com>
2025-05-12 06:27:09 +0200

    fixintro

--- docs/Demo/js/ui/introScreen.js
+++ docs/Demo/js/ui/introScreen.js
@@ -7,31 +7,24 @@ export const IntroScreen = {
     this.el = document.createElement('div');
     this.el.id = 'introScreen';
     this.el.innerHTML = `
-      <img id="titleImage" src="assets/media/titled-3.png" alt="Title Image" />
+      <img id="titleImage"
+           src="assets/media/titled-3.png"
+           style="box-shadow:none!important;filter:none!important;"
+           alt="Title Image" />
       <div id="continueText">Press any key or click to continue</div>
     `;
     document.body.appendChild(this.el);
 
-    // add listeners for keyboard and mouse input
     window.addEventListener('keydown', this._onInput);
     window.addEventListener('mousedown', this._onInput);
   },
   hide() {
-    // remove input event listeners and intro screen element
     window.removeEventListener('keydown', this._onInput);
     window.removeEventListener('mousedown', this._onInput);
     this.el.remove();
   },
-
-  /**
-   * this method is called when user presses key or clicks
-   * if intro overlay element is still present, do nothing
-   * otherwise navigate to main menu
-   */
-  _onInput(event) {
-    if (document.getElementById('introOverlay')) {
-      return; // waiting for intro overlay to disappear
-    }
+  _onInput() {
+    if (document.getElementById('introOverlay')) return; // wait until overlay fades
     advanceTo('MAIN_MENU');
   }
 };
